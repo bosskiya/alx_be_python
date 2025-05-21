d0d7c98e360f1077b236407e140a5061bf4ee256
binikiya <biniyamkiya3682@gmail.com>
2025-05-21 18:15:28 +0300

    Python Introduction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-x = input("Enter your monthly income: ")
-y = input("Enter your total monthly expenses: ")
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
-monthly_saving = float(x) - float(y)
+monthly_saving = float(monthly_income) - float(monthly_expenses)
 
 projected_saving = (monthly_saving * 12) + (monthly_saving * 12 * 0.05)
 
