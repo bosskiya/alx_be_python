8cf17e9bca0c24acdd3e62ba9ab168e54abc95bc
binikiya <biniyamkiya3682@gmail.com>
2025-06-11 20:12:28 +0300

    OOP, Exception, and Testing in Python

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,36 +8,40 @@ class TestSimpleCalculator(unittest.TestCase):
     """
     Unittest for Simple Calculator
     """
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
     def test_add_positive(self):
-        result = SimpleCalculator.add(self, 3, 4)
+        result = self.calc.add(3, 4)
         self.assertEqual(result, 7)
 
     def test_add_negative(self):
-        result = SimpleCalculator.add(self, -3, 4)
+        result = self.calc.add(-3, 4)
         self.assertEqual(result, 1)
 
     def test_subtract_positive(self):
-        result = SimpleCalculator.subtract(self, 3, 4)
+        result = self.calc.subtract(3, 4)
         self.assertEqual(result, -1)
     
     def test_subtract_negative(self):
-        result = SimpleCalculator.subtract(self, -3, 4)
+        result = self.calc.subtract(-3, 4)
         self.assertEqual(result, -7)
 
     def test_multiply_positive(self):
-        result = SimpleCalculator.multiply(self, 3, 4)
+        result = self.calc.multiply(3, 4)
         self.assertEqual(result, 12)
 
     def test_multiply_negative(self):
-        result = SimpleCalculator.multiply(self, -3, 4)
+        result = self.calc.multiply(-3, 4)
         self.assertEqual(result, -12)
 
     def test_divide_positive(self):
-        result = SimpleCalculator.divide(self, 6, 2)
+        result = self.calc.divide(6, 2)
         self.assertEqual(result, 3)
 
     def test_divide_positive(self):
-        result = SimpleCalculator.divide(self, 6, 0)
+        result = self.calc.divide(6, 0)
         self.assertEqual(result, None)
 
 if __name__ == "__main__":
